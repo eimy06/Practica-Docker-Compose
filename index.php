01b91ea732d72b569b2cda048816e6d5952017e7
eimy06 <eimylaurafigueroa@gmail.com>
2023-11-01 23:07:47 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola</title>
+</head>
+<body>
+    <h1>Eimy L. Figueroa, 2020-9190</h1>
+
+    <?php
+        $servername = getenv("MYSQL_HOST");
+        $username = getenv("MYSQL_USER");
+        $password = getenv("MYSQL_PASSWORD");
+        $database = getenv("MYSQL_DATABASE");
+
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        if ($conn->connect_error) {
+            die("Conexión a MySQL fallida: " . $conn->connect_error);
+        } else {
+            echo "Conexión a MySQL exitosa!";
+        }
+    ?>
+
+</body>
+</html>
